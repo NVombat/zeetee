ecc75fdaf0573ab59e9cf577051cb742f1541f08
NVombat <nvombatkere@gmail.com>
2024-10-07 17:09:24 -0600

    added common global variables; init: package structure with relative dot imports

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+from .helper import get_file_path
+
+
+# JSON File Paths
+default_jfp = get_file_path("testfiles", "rgp_hc.json")
+
+pos_jfp = get_file_path("testfiles", "rgp_test_pos.json")
+neg_jfp = get_file_path("testfiles", "rgp_test_neg.json")
+small_jfp = get_file_path("testfiles", "rgp_test_small.json")
+
+multi_jfp = get_file_path("testfiles", "rgp_hc_multiple.json")
+false_fmt_jfp = get_file_path("testfiles", "rgp_false_fmt.json")
+
+test_path_neg = get_file_path("testfiles", "rgp_gen_0.json")
+test_path_pos = get_file_path("testfiles", "rgp_gen_1.json")
