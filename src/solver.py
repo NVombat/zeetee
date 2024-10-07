e406fe139f3c25f1a3cbaab16a065755a0eb277f
NVombat <nvombatkere@gmail.com>
2024-10-07 17:11:27 -0600

    init: package structure with relative dot imports, finalised 2 solvers and structure

--- src/solver.py
+++ src/solver.py
@@ -1,33 +1,24 @@
 import sys
-from pysat.solvers import Glucose3, Solver
+from pysat.solvers import Glucose3, Lingeling
 
-from logger import create_logger
-from helper import get_file_path
-from er_encoder import rgp_to_sat_er
-from mb_encoder import rgp_to_sat_mb
-from converter import json_to_rgp, extract_clauses
+from . import *
+from .logger import create_logger
+from .er_encoder import rgp_to_sat_er
+from .mb_encoder import rgp_to_sat_mb
+from .converter import json_to_rgp, extract_clauses
 
 logger = create_logger(l_name="zt_solver")
-
-# JSON File Paths
-pos_jfp = get_file_path("testfiles", "rgp_test_pos.json")
-neg_jfp = get_file_path("testfiles", "rgp_test_neg.json")
-small_jfp = get_file_path("testfiles", "rgp_test_small.json")
-
-test_path_neg = get_file_path("testfiles", "rgp_gen_0.json")
-test_path_pos = get_file_path("testfiles", "rgp_gen_1.json")
 
 
 def solve(enc_type: int, solver_flag: int, rgp_instance: dict) -> dict:
     '''
-    Takes an encoding type and solver type passed
-    along with an RGP instance. It converts the
-    input to a solvable form which it then solves
-    using a SAT solver
+    Takes an encoding type and solver type passed along with
+    an RGP instance. It converts the input to a solvable
+    form which it then solves using a SAT solver
 
     Args:
         enc_type: [1(Encoding 1), 2(Encoding 2)]
-        solver_flag: [1-10] for the different types of solvers
+        solver_flag: [1 (Glucose3), 2(Lingeling)]
         rgp_instance: RGP Instance
 
     Returns:
@@ -36,11 +27,11 @@ def solve(enc_type: int, solver_flag: int, rgp_instance: dict) -> dict:
     logger.debug(f"RGP Instance: {inst}")
 
     try:
-        if enc_type not in range(1,3) or solver_flag not in range(1,11):
+        if enc_type not in range(1,3) or solver_flag not in range(1,3):
             raise ValueError
 
     except ValueError:
-        logger.error("Encoding Type must be an integer and must be either 1 or 2. Solver Flag must be an integer and must be between 1 and 10")
+        logger.error("Usage: python solver.py enc_type solver [enc_type must be an integer and must be either 1 or 2; solver must be an integer and must be either 1 or 2]")
         sys.exit(1)
 
     logger.info(f"Encoding Type is set to {enc_type}... Solver is set to {solver_flag}...")
@@ -55,28 +46,40 @@ def solve(enc_type: int, solver_flag: int, rgp_instance: dict) -> dict:
         sat_obj = rgp_to_sat_er(rgp_instance)
         logger.info(f"SAT Object [ER-ENCODER]: {sat_obj}")
 
-    clauses = extract_clauses(sat_obj)
-
-    logger.debug(f"Solving Instance...")
-
-    # Add Solver Switch Case [1-10]
-
-    # Initialize SAT Solver
-    solver = Glucose3()
-    # solver = Solver()
-
-    for clause in clauses:
-        solver.add_clause(clause)
-
-    satisfiable = solver.solve()
-    logger.debug(f"Satisfiable: {satisfiable}")
-
-    if satisfiable:
-        logger.debug(f"Solution: {solver.get_model()}")
-    else:
-        logger.debug("No satisfiable solution exists!")
-
     res = {}
+
+    if sat_obj["unsatisfiable"]:
+        satisfiable = False
+
+    else:
+        clauses = extract_clauses(sat_obj)
+
+        logger.debug(f"Solving Instance...")
+
+        # Initialize SAT Solver
+        if solver_flag == 1:
+            # Glucose3
+            logger.debug("Solver: Glucose3")
+            solver = Glucose3()
+
+        elif solver_flag == 2:
+            # Lingeling
+            logger.debug("Solver: Lingeling")
+            solver = Lingeling()
+
+        for clause in clauses:
+            solver.add_clause(clause)
+
+        satisfiable = solver.solve()
+        logger.debug(f"Satisfiable: {satisfiable}")
+
+        if satisfiable:
+            logger.debug(f"Solution: {solver.get_model()}")
+        else:
+            logger.debug("No satisfiable solution exists!")
+
+        solver.delete()
+
     res["status"] = satisfiable
     res["result"] = []
 
@@ -88,7 +91,7 @@ if __name__ == "__main__":
     cl_args = sys.argv
 
     if len(cl_args) != 3:
-        logger.error("Usage: python solver.py enc_type solver [enc_type must be an integer and must be either 1 or 2; solver must be an integer and must be between 1 and 10]")
+        logger.error("Usage: python solver.py enc_type solver [enc_type must be an integer and must be either 1 or 2; solver must be an integer and must be either 1 or 2]")
         sys.exit(1)
 
     logger.debug(f"Command Line Args: {cl_args}")
@@ -97,11 +100,11 @@ if __name__ == "__main__":
         enc_type = int(cl_args[1])
         slv_flag = int(cl_args[2])
 
-        if enc_type not in range(1,3) or slv_flag not in range(1,11):
+        if enc_type not in range(1,3) or slv_flag not in range(1,3):
             raise ValueError
 
     except ValueError:
-        logger.error("Usage: python solver.py enc_type solver [enc_type must be an integer and must be either 1 or 2; solver must be an integer and must be between 1 and 10]")
+        logger.error("Usage: python solver.py enc_type solver [enc_type must be an integer and must be either 1 or 2; solver must be an integer and must be either 1 or 2]")
         sys.exit(1)
 
     logger.info(f"Encoding Type is set to {enc_type}... Solver is set to {slv_flag}...")
