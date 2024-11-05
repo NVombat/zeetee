024d55287fbdaa32a53fa70679d76d106fb9b530
NVombat <nvombatkere@gmail.com>
2024-11-04 19:20:39 -0700

    added a experiment script - to run the experiment

---
+++ src/arc_runner.py
@@ -0,0 +1,14 @@
+from .logger import create_logger
+from . import experiment_config_path
+from .runner import get_experiment_config_and_run_experiment
+
+logger = create_logger(l_name="zt_arc_runner")
+
+if __name__ == "__main__":
+    logger.info("********************ARC_RUNNER[RUNNING EXPERIMENT]*********************")
+
+    logger.info(f"Experiment Configuration Path: {experiment_config_path}")
+
+    get_experiment_config_and_run_experiment(experiment_config_path, run_serially=False, plot_results=True, run_existing=False)
+
+    logger.info("Experiment Run Complete...")
