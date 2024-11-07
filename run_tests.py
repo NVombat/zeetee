a8869e21e76358f5f5656bfde04b6687913d6d56
NVombat <nvombatkere@gmail.com>
2024-11-06 19:03:44 -0700

    added SECONDARY_EMAIL and updated test suite to include new file modules (TESTS to be written...)

--- run_tests.py
+++ run_tests.py
@@ -1,23 +1,25 @@
 from tests import (
-    test_converter,
     test_er_encoder,
     test_format_validator,
     test_generator,
     test_helper,
     test_mb_encoder,
-    test_solver
+    test_runner,
+    test_solver,
+    test_utils
 )
 
 def get_unittests(suite):
-    suite.addTest(unittest.makeSuite(test_converter.TestConverter))
     suite.addTest(unittest.makeSuite(test_format_validator.TestFormatValidator))
     suite.addTest(unittest.makeSuite(test_generator.TestGenerator))
     suite.addTest(unittest.makeSuite(test_helper.TestHelper))
+    suite.addTest(unittest.makeSuite(test_utils.TestUtils))
 
 def get_encoder_tests(suite):
     suite.addTest(unittest.makeSuite(test_er_encoder.TestEREncoder))
     suite.addTest(unittest.makeSuite(test_mb_encoder.TestMBEncoder))
     suite.addTest(unittest.makeSuite(test_solver.TestSolver))
+    suite.addTest(unittest.makeSuite(test_runner.TestRunner))
 
 def main():
     suite = unittest.TestSuite()
